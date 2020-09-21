9ee14e5b0da705bc69618b765baafe10430f581a
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-09-22 00:25:42 +0530

    Update bagging.py

--- bagging.py
+++ bagging.py
@@ -1,4 +1,4 @@
-# Bagging Algorithm on the Sonar dataset
+# Bagging Algorithm 
 from random import seed
 from random import randrange
 from csv import reader
@@ -187,23 +187,23 @@ def bagging(train, test, max_depth, min_size, sample_size, n_trees):
 	predictions = [bagging_predict(trees, row) for row in test]
 	return(predictions)
 
-# Test bagging on the sonar dataset
-seed(1)
-# load and prepare data
-filename = 'sonar.all-data.csv'
-dataset = load_csv(filename)
-# convert string attributes to integers
-for i in range(len(dataset[0])-1):
-	str_column_to_float(dataset, i)
-# convert class column to integers
-str_column_to_int(dataset, len(dataset[0])-1)
-# evaluate algorithm
-n_folds = 5
-max_depth = 6
-min_size = 2
-sample_size = 0.50
-for n_trees in [1, 5, 10, 50]:
-	scores = evaluate_algorithm(dataset, bagging, n_folds, max_depth, min_size, sample_size, n_trees)
-	print('Trees: %d' % n_trees)
-	print('Scores: %s' % scores)
-	print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+# # Test bagging on the sonar dataset
+# seed(1)
+# # load and prepare data
+# filename = 'sonar.all-data.csv'
+# dataset = load_csv(filename)
+# # convert string attributes to integers
+# for i in range(len(dataset[0])-1):
+# 	str_column_to_float(dataset, i)
+# # convert class column to integers
+# str_column_to_int(dataset, len(dataset[0])-1)
+# # evaluate algorithm
+# n_folds = 5
+# max_depth = 6
+# min_size = 2
+# sample_size = 0.50
+# for n_trees in [1, 5, 10, 50]:
+# 	scores = evaluate_algorithm(dataset, bagging, n_folds, max_depth, min_size, sample_size, n_trees)
+# 	print('Trees: %d' % n_trees)
+# 	print('Scores: %s' % scores)
+# 	print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
